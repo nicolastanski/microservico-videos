2ed227e672cd5b367795b30df0636b4bb3115b6b
Nicolas Tanski Pavão <nicolas.tanski@gmail.com>
2020-11-20 15:49:20 -0300

    Category and Gender resources

---
+++ app/Http/Controllers/Api/GenderController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Gender;
+use Illuminate\Http\Request;
+
+class GenderController extends Controller
+{
+    private $rules = [
+        'name' => 'required|max:255',
+        'is_active' => 'boolean'
+    ];
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Gender::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, $this->rules);
+        return Gender::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Gender  $gender
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Gender $gender)
+    {
+        return $gender;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Gender  $gender
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Gender $gender)
+    {
+        $this->validate($request, $this->rules);
+        $gender->update($request->all());
+        return $gender;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Gender  $gender
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Gender $gender)
+    {
+        $gender->delete();
+        return response()->noContent();
+    }
+}
---
+++ database/seeds/GenderTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Models\Gender;
+use Illuminate\Database\Seeder;
+
+class GenderTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Gender::class, 100)->create();
+    }
+}
